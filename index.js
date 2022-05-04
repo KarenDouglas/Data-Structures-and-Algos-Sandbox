c2e8c66f0a864500faf878baab3a6a5b80fddeab
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2022-05-04 16:53:50 +0000

    Big O(n)

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const nemo = [ 'dory', 'charles', 'brody', 'twitch','nemo'];
 const large = new Array(10000).fill('nemo')
 
 function findNemo(array) {
-   let t0 = performance.now();
   for(let i = 0 ; i < array.length; i++){
 
     if(array[i] === 'nemo'){
       console.log('found NEMO')
     }
   }
-  let t1 = performance.now();
 
-  console.log('Call to find Nemo took '+ (t1-t0) + ' milliseconds');
 }
 
 findNemo(large);
+// how does this function grow as the input increases? :   for every element in the array, there is an operation
+// O(n) ---It takes linear time to find nemo... so the Big O depends on the number of inputs. This is the most common type of complexity
+// n = size of the input 
