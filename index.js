24dee3dddcb7b5a7d6ca217bb7a8f3860dfe8725
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2022-05-10 17:22:57 +0000

    exercise- space complexity

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-// function compressFirstBox(boxes) {
-//   console.log(boxes[0])
-// }
-// O(1)--- Constant Time
-
-const boxes = [1,2,3,4,5];
-
-function logFirstTwoBoxes(boxes){
-  console.log(boxes[0]) // O(1)
-  console.log(boxes[1])// O(1)
+function booo(n) {
+  // the only additional spaced used in this function is: let i = 0
+  for (let i = 0; i < n.length; i++) {
+    console.log('boo')
+  }
 }
 
-logFirstTwoBoxes(boxes);// O(2)
 
-// it doesn't matter how many inputs there are when it is constant time label it is O(n)
+
+booo([1, 2, 3, 4, 5])
+// time complexity o(n)
+//space complexity only considers the ADDITIONAL space used. it does not consider the space used by the inputs.
+
+// this function has the space complexity of 0(1)
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6)// O(n) space complexity
