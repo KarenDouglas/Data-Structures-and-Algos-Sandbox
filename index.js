c2711a46d204a16d84a08d76af58e537c5510891
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2022-05-12 18:21:36 +0000

    arrays intro

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
-// Given 2 arrays, create a func that let's user know whether these two arrays contain any common items:
+const strings = ['a', 'b','c','d'];
 
-const array1= ['a','b','c','x'];
-const array2= ['z','y','i'];
-const array3= ['a','b','c', 'x'];
-const array4= ['z','y','x'];
+// 4 items on 4 shelves of storage in RAM takes up 16bytes of storage
 
-function hasMatchingItem(arr1, arr2){
+// push O(1)
+// pop O(1)
+//unshift  O(n) because, not only is the first element added to the array, but also the index # for all of the elements in the array will change. Which is a loop.
 
-  for(let i = 0; i< arr1.length; i++){
-    for (let j =0; j< arr2.length; j++){
-      if(arr1[i]===arr2[j]){
-        return true;
-      }
-    }
-  }
-  return false
-}
+// splice  (start #, delete count, items to add)
+strings.splice(2,0, 'alien')// O(n)
 
-
-console.log('with for loops: '+
-  hasMatchingItem(array4,array1)
-)
-
-
-// usually  for loops can be turned into hash tables
-// 1. First I will true my array into an object
-// array1 => obj {
-// a:true,
-// b:true,
-// c:true,
-// x:true,
-// }
-
-// 2. Then I will check if the second array is equal to the objects properties
-
-// array2[index] ===obj.properties
-
-function containsCommonItem(arr1,arr2){
-  // loop thru first array and create object where props  === items in the array
-  // loop thru secon array and check if item in 2nd array exists on created object
-
-  let map = {};
-
- for( let i = 0; i< arr1.length; i++){
-   if (!map[[arr1[i]]]){
-     const item= arr1[i];
-     map[item] = true;
-   }
- }
- for (let j = 0 ; j< arr2.length; j++){
-   if(map[arr2[j]]){
-     return true;
-   }
- }
-  return false
-}
-console.log(
-containsCommonItem(array3, array4)
-  
-)
+console.log(strings)
